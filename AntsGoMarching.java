9806aea3fcaacb2d25d79c76fb8b6b8532f5643a
Ian Thompson <ianthompson@nicelion.com>
2017-08-28 10:29:04 -0400

    added full "Ants Go Marching" lyrics

--- AntsGoMarching.java
+++ AntsGoMarching.java
@@ -1,71 +1,95 @@
 /**
  * This program sings the song "Ants Go Marching."
- * lyrics: http://kids.niehs.nih.gov/lyrics/antsgo.htm
+ * lyrics: http://www.kidspot.com.au/Baby-Baby-play-and-toys-The-ants-go-marching-lyrics+5210+425+article.htm
  *
- * @author  you
+ * @author  Ian Thompson
  * @version today
  */
 
 public class AntsGoMarching 
 {
-	/**
-	 * Sings the first two lines.
-	 *
-	 * @param Number (as a word) for how the ants march
-	 */
-	public static void goMarching(String number) 
-	{
+    /**
+     * Sings the first two lines.
+     *
+     * @param number (as a word) for how the ants march
+     */
+    public static void goMarching(String number) 
+    {
+            System.out.println("The ants gor marching " + number + " by " + number);
+            System.out.println("Hoorah! Horrah!");
+            
+    }
+    
+    /**
+     * Sings the third line.
+     *
+     * @param number (as a word) for how the ants march
+     */ 
+    public static void noHurrah(String number)
+    {
+        System.out.println("The ants go marching " + number + " by " + number);
+    }
+    
+    /**
+     * Sings the 4th line about the little ant.
+     *
+     * @param action What the little one stops to do
+     */
+    public static void littleAnt(String action)
+    {   
+        System.out.println("The little one stops to " + action + ",");
+        System.out.println("And they all go marching down into the ground");
+        
+    }
+    
+    /**
+     * Sings the last two lines of each verse.
+     */ 
+    public static void rain() 
+    {
+        System.out.println("To get out of the rain.");
+        System.out.println("Boom, boom, boom, boom!");
 
-	}
-	
-	/**
-	 * Sings the third line.
-	 *
-	 * @param Number (as a word) for how the ants march
-	 */	
-	public static void noHurrah(String number)
-	{
+    }
 
-	}
-	
-	/**
-	 * Sings the 4th line about the little ant.
-	 *
-	 * @param action What the little one stops to do
-	 */
-	public static void littleAnt(String action)
-	{	
+    /**
+     * Sings a verse of Ants Go Marching.
+     * 
+     * @param number Number (as a word) for how the ants march
+     * @param action What the little one stops to do
+     */
+    public static void singIt(String n, String action) 
+    {
+        goMarching(n);
+        goMarching(n);
+        
+        noHurrah(n);
+        
+        littleAnt(action);
+        rain();
+        
+        System.out.println();
+    }
 
-	}
-	
-	/**
-	 * Sings the last two lines of each verse.
-	 */	
-	public static void rain() 
-	{
-
-	}
-
-	/**
-	 * Sings a verse of Ants Go Marching.
-	 * 
-	 * @param number Number (as a word) for how the ants march
-	 * @param action What the little one stops to do
-	 */
-	public static void singIt(String n, String action) 
-	{
-
-		
-	}
-
-	/**
-	 * Calls the singIt() for each verse.
-	 */
-	public static void main(String[] args) 
-	{
-		System.out.println("Ants Go Marching\n");
-		
-		singIt("one","suck his thumb");
-	}
+    /**
+     * Calls the singIt() for each verse.
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Ants Go Marching\n");
+        
+        singIt("one","suck his thumb");       
+        singIt("two", "tie his shoe");
+        singIt("three", "climb a tree");
+        singIt("four", "shut the door");
+        singIt("five", "take a dive");
+        singIt("six", "pick up sticks");
+        singIt("seven", "pray to heaven");
+        singIt("eight", "rollerskate");
+        singIt("nine", "check the time");
+        singIt("ten", "shout THE END");
+        
+        
+    }
    
 }
